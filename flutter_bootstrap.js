cb9e99a9a84d2b2cbc827245b3c4cbbeec4ce6f6
keerthirajan23 <192471042.simats@saveetha.com>
2026-06-26 08:45:43 +0000

    Deploying to gh-pages from @ harish18AG/Mospl-FInal@af7eb5c96c825e5eba327c380380082aa6ae714b 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2484055465" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1510166419" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
